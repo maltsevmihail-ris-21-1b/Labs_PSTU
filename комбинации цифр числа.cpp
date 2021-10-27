8f4b5bcba389e982bfd1cd91f925340e039dc6b5
Михаил Мальцев <70252426+maltsevmihail-ris-21-1b@users.noreply.github.com>
2021-10-27 21:45:23 +0500

    Update комбинации цифр числа.cpp

--- комбинации цифр числа.cpp
+++ комбинации цифр числа.cpp
@@ -1,36 +1,24 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    setlocale(0,"");
+    setlocale(0, "");
     int x;
-    int a,b,c;
+    int a, b, c;
     bool f = false;
     cout << "Введите трехзначное число: ";
-    cin >> x;
-    while (x < 100 || x > 999) {
-        cout << "\nНеверный ввод. Введите трехзначное число: ";
+    do {
         cin >> x;
-    }
-    a = x % 10;
-    x /= 10;
-    b = x % 10;
-    c = x / 10;
-    while (f == false) {
-        if ((a == b)||(b == c)||(c == a)) {
-            cout << "Неверный ввод: цифры в числе не должны повторяться.\nВведите трехзначное число: ";
-            cin >> x;
-            a = x % 10;
-            x /= 10;
-            b = x % 10;
-            c = x /10;
-        }
+        a = x % 10;
+        b = (x / 10) % 10;
+        c = x / 100;
+        if (x < 100 || x > 999 || a == b || b == c || c == a)
+            cout << "Неверный ввод.\nВведите трехзначное число без повторяющихся цифр: ";
         else 
             f = true;
-    }
-    cout << "Все возможные комбинации чисел: \n";
+    } while (f == false);
+    cout << "Все возможные комбинации чисел: ";
     cout << a << b << c << endl;
     cout << a << c << b << endl;
     cout << b << c << a << endl;
@@ -43,7 +31,7 @@ int main()
             cout << a << b << c;
         else
             cout << a << c << b;
-    else if (b > a && b >c)
+    else if (b > a && b > c)
         if (a > c)
             cout << b << a << c;
         else
