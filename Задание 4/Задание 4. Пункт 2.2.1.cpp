b1b49185934506c766c37389121dc9a21f57842d
Михаил Мальцев <70252426+maltsevmihail-ris-21-1b@users.noreply.github.com>
2021-11-04 14:07:54 +0500

    Update Задание 4. Пункт 2.2.1.cpp

--- Задание 4/Задание 4. Пункт 2.2.1.cpp
+++ Задание 4/Задание 4. Пункт 2.2.1.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
-using namespace std;
-float f;
-int main()
+#include <iostream> //подключение библиотек
+using namespace std; //подключение пространства имён
+float f; //объявление переменной
+int main() //главная функия
 {
-    cin >> f;
-    cout << (f-32)*5/9;
+    cin >> f; //ввод переменной
+    cout << (f-32)*5/9; //вывод результата
     return 0;
 }
